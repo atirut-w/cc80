a57834261a9ef1f9726e89af91f17e206cf371d1
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-06-15 21:08:06 +0700

    Add file loading

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from argparse import ArgumentParser, Namespace
 
 
 def main(args: Namespace) -> int:
+    with open(args.input, "r") as f:
+        print(f.read())
     return 0
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(prog="CC80", description="C to Z80 ASM compiler")
+    parser.add_argument("input", help="Input file")
 
     exit(main(parser.parse_args()))
